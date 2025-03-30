b33a5de5d69ac550e269b3b5ae9a276d870835c5
Kirill <kirill.ustimencko@gmail.com>
2025-03-30 23:59:45 +0300

    added remove expired urls scheduler

--- src/url-shortener/main.py
+++ src/url-shortener/main.py
@@ -1,8 +1,9 @@
-import logging
+import logging, uvicorn, os
 from database.database import run_migrations
 from fastapi import FastAPI
 from api.routes.urls import router as url_router
-import uvicorn, os
+from apscheduler.schedulers.background import BackgroundScheduler
+from scheduler.scheduled_task import remove_expired_urls
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -20,3 +21,7 @@ if __name__ == "__main__":
    port = int(os.getenv('BACKEND_PORT'))
    log.info("Starting web-server with port = [%s]", port)
    uvicorn.run(app, host="0.0.0.0", port=port)
+   
+   scheduler = BackgroundScheduler()
+   scheduler.add_job(remove_expired_urls, 'interval', seconds=30)
+   scheduler.start()
