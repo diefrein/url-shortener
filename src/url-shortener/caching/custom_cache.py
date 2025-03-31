7970e528b4cf509ce4130eab162d0f2c9410843c
Kirill <kirill.ustimencko@gmail.com>
2025-03-31 20:58:12 +0300

    added cache evicting

--- src/url-shortener/caching/custom_cache.py
+++ src/url-shortener/caching/custom_cache.py
@@ -15,20 +15,41 @@ def clear_cache_for_function(func, *args, **kwargs):
     cache_key = f"{func.__name__}:{str(args)}:{str(kwargs)}"
     redis_client.delete(cache_key)
     
+def invalidate_cache(pattern=None, all_keys=False):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if all_keys:
+                log.info(f"flushdb")
+                redis_client.flushdb()
+            elif pattern:
+                keys = redis_client.keys(pattern)
+                log.info(f"keys = {keys}")
+                if keys:
+                    redis_client.delete(*keys)
+            else:
+                raise RuntimeError("pattern cannot be null if all_keys is False")
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+    
 def cache_query(ttl=3600):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            cache_key = f"sql_cache:{func.__name__}:{str(args[1:])}:{str(kwargs)}"
+            func_kwargs = kwargs.copy()
+            func_kwargs.pop('session', None)
+            
+            cache_key = f"{func.__name__}:{str(args)}:{str(func_kwargs)}"
             
             cached_data = redis_client.get(cache_key)
             if cached_data:
-                log.info(f"Getting data from cache, cache_key = {cache_key}")
+                log.debug(f"Getting data from cache, cache_key = {cache_key}")
                 return pickle.loads(cached_data)
             
             result = func(*args, **kwargs)
             
-            log.info(f"Сaching data, cache_key = {cache_key}")
+            log.debug(f"Сaching data, cache_key = {cache_key}")
             redis_client.setex(cache_key, ttl, pickle.dumps(result))
             return result
         return wrapper
--- src/url-shortener/database/database.py
+++ src/url-shortener/database/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session, sessionmaker
 import logging, uuid
 from urlgenerator.generator import generate_short_url
 from datetime import datetime
-from caching.custom_cache import cache_query
+from caching.custom_cache import cache_query, invalidate_cache
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -79,6 +79,7 @@ def delete_user(*, session: Session, user_id: uuid):
         log.error(f"Exception while deleting user with id = {user_id}", e)
         session.rollback()
         
+@invalidate_cache(pattern='get_urls*')
 def create_url(*, session: Session, url_create: UrlCreate) -> Url:
     try:
         short_url = generate_short_url(full_url=url_create.full_url)
@@ -98,6 +99,7 @@ def create_url(*, session: Session, url_create: UrlCreate) -> Url:
         log.error(f"Exception while creating url", e)
         session.rollback() 
         
+@invalidate_cache(pattern='get_urls*')
 def create_url(*, session: Session, url: Url) -> Url:
     try:
         session.add(url)
@@ -109,6 +111,7 @@ def create_url(*, session: Session, url: Url) -> Url:
         log.error(f"Exception while creating url", e)
         session.rollback() 
         
+@invalidate_cache(pattern='get_urls*')
 def delete_url(*, session: Session, url_id: uuid):
     try:
         url = session.query(Url).filter_by(id=url_id).first()
@@ -138,6 +141,7 @@ def get_urls(*, session: Session, ids: list = None, short_url: str = None, full_
         log.error(f"Exception while getting urls", e)
         session.rollback()
         
+@invalidate_cache(pattern='get_urls*')
 def update_url_use_count(*, session: Session, url_id: uuid):
     try:
         session.execute(
@@ -153,6 +157,7 @@ def update_url_use_count(*, session: Session, url_id: uuid):
         log.error(f"Exception while updating url use count for url with id = {url_id}", e)
         session.rollback()
         
+@invalidate_cache(pattern='get_urls*')
 def update_url(*, session: Session, url: Url) -> Url:
     try:
         session.add(url)
@@ -163,6 +168,7 @@ def update_url(*, session: Session, url: Url) -> Url:
         log.error(f"Exception while updating with id = {url.id}", e)
         session.rollback()
         
+@invalidate_cache(pattern='get_urls*')
 def remove_expired_urls(*, session: Session):
     try:
         session.execute(text(
